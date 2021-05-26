8cb9a6091ccd06fc5317fc01e7bed7e279d5181d
Joseph Saheeb <sahjome@gmail.com>
2021-05-26 17:19:54 +0100

    testing fp functions

--- HOGRegister/Controllers/HomeController.cs
+++ HOGRegister/Controllers/HomeController.cs
@@ -3,13 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SecuGen.FDxSDKPro.Windows;
+
 
 namespace HOGRegister.Controllers
 {
     public class HomeController : Controller
     {
+        private SGFingerPrintManager m_FPM; //member variable
+        SGFPMDeviceName device_name = SGFPMDeviceName.DEV_FDU03;
+        static int m_ImageWidth, m_ImageHeight;
+
+        public void InitializeSGFingerPrintManager()
+        {
+            m_FPM = new SGFingerPrintManager();
+            int port_addr;
+            port_addr = (int)SGFPMPortAddr.USB_AUTO_DETECT;
+            int iError = m_FPM.OpenDevice(port_addr);
+            if (iError == (int)SGFPMError.ERROR_NONE)
+            {
+                SGFPMDeviceInfoParam pInfo = new SGFPMDeviceInfoParam();
+                pInfo = new SGFPMDeviceInfoParam();
+                iError = m_FPM.GetDeviceInfo(pInfo);
+                if (iError == (Int32)SGFPMError.ERROR_NONE)
+                {
+                    // This should be done GetDeviceInfo();
+                    m_ImageWidth = pInfo.ImageWidth;
+                    m_ImageHeight = pInfo.ImageHeight;
+                }
+                ViewBag.Message = "OpenDevice() Error : " + iError;
+            }
+            else
+                ViewBag.Message = "OpenDevice() Error : " + iError;
+
+        }
+
+        //getting finger print image
+        public void GetFingerPrintImage()
+        {
+            byte[] fp_image = new Byte[m_ImageWidth * m_ImageHeight];
+            int iError;
+            iError = m_FPM.GetImage(fp_image);
+            if (iError == (int)SGFPMError.ERROR_NONE)
+            {
+                //ToDO: pass the fingerprint image to the screen
+                //DrawImage(fp_image, pictureBox1);
+            }
+            else
+                ViewBag.Message = "GetImage() Error : " + iError;
+        }
+
+        //void OpenFingerPrint
+
         public ActionResult Index()
         {
+            InitializeSGFingerPrintManager();
             return View();
         }
 
